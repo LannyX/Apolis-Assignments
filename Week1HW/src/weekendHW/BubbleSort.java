b576b7b72821b09130a6bd295c43e3f6d8eaa331
Lanny <xulanyixuanr@gmail.com>
2018-12-17 13:57:31 -0600

    week2 commit

--- Week1HW/src/weekendHW/BubbleSort.java
+++ Week1HW/src/weekendHW/BubbleSort.java
@@ -25,7 +25,7 @@ public class BubbleSort {
 		
 		for (int i = 0; i < n ; i++){
 			for (int j = 1; j < n - i; j++){
-				if(arr[j -1 ] > arr[j]){
+				if(arr[j - 1 ] > arr[j]){
 					temp = arr[j - 1];
 					arr[j - 1] = arr[j];
 					arr[j] = temp;
